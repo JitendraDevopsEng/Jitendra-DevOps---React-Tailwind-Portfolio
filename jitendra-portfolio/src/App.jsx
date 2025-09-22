70a01e8728dcd5c93c8d3d7db484f0e8f4c73ef7
Jitendra DevOps Engineer <Jitendra.devops.eng@gmail.com>
2025-09-22 21:41:54 +0530

    background updated

--- jitendra-portfolio/src/App.jsx
+++ jitendra-portfolio/src/App.jsx
@@ -6,29 +6,24 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import ParticlesBackground from "./components/ParticlesBackground";
 
 export default function App() {
   return (
-    <div className="relative min-h-screen text-white">
-      {/* 🌅 Animated Saffron Background */}
-      <div className="absolute inset-0 bg-saffron-animated"></div>
-
-      {/* 🌓 Overlay for readability */}
-      <div className="absolute inset-0 bg-black/30"></div>
-
-      {/* 🌐 Content */}
-      <div className="relative z-10">
-        <Navbar />
-        <main>
-          <Hero />
-          <About />
-          <Experience />
-          <Skills />
-          <Projects />
-          <Contact />
-        </main>
-        <Footer />
-      </div>
+    <div className="bg-gray-900 text-white min-h-screen relative">
+      {/* 👈 Background sabse pehle */}
+      <ParticlesBackground />  
+      
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Experience />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
     </div>
   );
 }
